0f848cdec9e97eb6725a946738069317ac4b4f75
TERAN-XMD-maker <teranxd11@gmail.com>
2025-08-28 16:27:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,4 @@
-  </style>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f4f4f4;
-      margin: 0;
-      padding: 0;
-    }
-    header {
-      background: #333;
-      color: #fff;
-      padding: 1em;
-      text-align: center;
-    }
-    main {
-      padding: 2em;
-      text-align: center;
-    }
-    button {
-      background: #007bff;
-      color: #fff;
-      padding: 0.8em 1.5em;
-      border: none;
-      border-radius: 5px;
-      font-size: 1em;
-      cursor: pointer;
-    }
-    button:hover {
-      background: #0056b3;
-    }
-  </style>
-
-  <!-- PushAlert -->
+<-- PushAlert -->
   <script type="text/javascript">
   (function(d, t) {
       var g = d.createElement(t),
